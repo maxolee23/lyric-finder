e52b172263c1b0434f68bfffd9cd0d8d7a427b9b
maxolee23 <maxolee23@gmail.com>
2020-03-23 03:01:44 -0500

    initial commit

---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from './Track'
+
+class Tracks extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          const { trackList, heading } = value;
+          if (trackList === undefined || trackList.length === 0) {
+            return <Spinner />;
+          } else {
+            return (
+              <React.Fragment>
+          <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                    {trackList.map(item => (
+                        <Track key={item.track.track_id} track={item.track}/>
+                    ))}
+                </div>
+              </React.Fragment>
+            );
+          }
+        }}
+      </Consumer>
+    );
+  }
+}
+
+export default Tracks;
